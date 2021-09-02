e7f4ca2dd2cb85b8e1e8a3b178c5977322ff126a
13025 <13025@128.4.99.248>
2021-09-02 17:53:22 -0400

    KMN

--- HW/Hello.java
+++ HW/Hello.java
@@ -1,6 +1,6 @@
 public class Hello{
 	public static void main(String[] args) {
 		System.out.println("Hello there");
-		System.out.println("I'm so done")
+		System.out.println("I'm so done");
 	}
 }
